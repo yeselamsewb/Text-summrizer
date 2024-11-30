bbb4eea3e50ba4888506888fd9fd653638af4c42
Yeselamsew_b <149420757+yeselamsewb@users.noreply.github.com>
2024-11-30 23:15:22 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,26 @@
+const callAPI =() => {
+    console.log("callAPI");
+    const mhh = "hf"
+    const paragraph = document.getElementById("text-input");
+    const text = paragraph.value;
+    const output = document.getElementById("text-output");
+    async function query(data) {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+            {
+                headers: {
+                    Authorization: "Bearer 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",
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify(data),
+            }
+        );
+        const result = await response.json();
+        return result;
+    }
+    
+    query({inputs: text}).then((response) => {
+        output.innerHTML = response[0]?.summary_text
+    });
+};
