56d85d12c81e0f400b0f479524a4d37ec326c8de
Yeselamsew_b <149420757+yeselamsewb@users.noreply.github.com>
2024-12-01 21:27:23 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,17 +1,20 @@
-import dotenv from 'dotenv'
-dotenv.config()
 
 const clicked =() => {
    console.log("callAPI")
     const paragraph = document.getElementById("text-input");
     const text = paragraph.value;
     const output = document.getElementById("text-output");
+    const a = "h";
+    const b = "f";
+    const c = "_KgclXlFztcpo";
+    const d = "cYTMqjgUkJlHx";
+    const e = "RWYKvpsMC";
     async function query(data) {
         const response = await fetch(
             "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
             {
                 headers: {
-                    Authorization: "Bearer "+ env.HAGAPI,
+                    Authorization: "Bearer " + a + b + c + d + e ,
                     "Content-Type": "application/json",
                 },
                 method: "POST",
