91147c5c89f8d48b7fe080711a169fd76e2cc1b0
Yeselamsew_b <149420757+yeselamsewb@users.noreply.github.com>
2025-07-08 03:48:13 -0400

    Update main.js

--- script/main.js
+++ script/main.js
@@ -10,22 +10,21 @@ const clicked =() => {
     const d = "bIVMaxHuuKw";
     const e = "FQwOYTntUEp";
     async function query(data) {
-        const response = await fetch(
-            "https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B-Instruct",
-            {
-                headers: {
-                    Authorization: "Bearer " + a + b + c + d + e ,
-                    "Content-Type": "application/json",
-                },
-                method: "POST",
-                body: JSON.stringify(data),
-            }
-        );
-        const result = await response.json();
-        return result;
-    }
-    
-    query({inputs: text}).then((response) => {
-        output.innerHTML = response[0]?.summary_text
-    });
-}; 
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
+        {
+            headers: {
+                Authorization: "Bearer " + a + b + c + d + e,
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+
+query({ inputs: text }).then((response) => {
+    output.innerHTML = response?.generated_text || "No response";
+});
